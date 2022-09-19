bf07b5e28fef98e8ef1d91a7216e1a333d45af82
Bastiloko <bastianandresxb@gmail.com>
2022-09-19 11:48:51 -0300

    chore: added post request in form data

--- requests/fetch.js
+++ requests/fetch.js
@@ -1,7 +1,13 @@
 const axios = require("axios");
 const BASE_URL = "https://api.chainstack.com/v1";
 
-const fetchWithToken = async (endpoint, data, token, method = "get") => {
+const fetchWithToken = async (
+  endpoint,
+  data,
+  token,
+  method = "get",
+  headers
+) => {
   const url = `${BASE_URL}/${endpoint}`;
 
   try {
@@ -29,4 +35,24 @@ const fetchWithToken = async (endpoint, data, token, method = "get") => {
   }
 };
 
-module.exports = { fetchWithToken };
+fetchWithTokenFormData = async (endpoint, form, token) => {
+  const url = `${BASE_URL}/${endpoint}`;
+
+  try {
+    return await axios(url, {
+      method: "post",
+      headers: {
+        "Content-type": "multipart/form-data",
+        Authorization: `Bearer ${token}`,
+      },
+      data: form,
+    });
+  } catch (error) {
+    return {
+      status: error.response.status,
+      message: `[ERROR] ${error.response.data.error.code}: ${error.response.data.error.message}`,
+    };
+  }
+};
+
+module.exports = { fetchWithToken, fetchWithTokenFormData };
